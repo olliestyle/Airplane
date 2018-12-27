feb6125ff1530b779a8cdc5f9893a28394049e3f
olliestyle <olliestyle1989@gmail.com>
2018-12-27 16:39:07 +0300

    Lifecycle comments

--- core/src/com/airplane/game/AirplaneGame.java
+++ core/src/com/airplane/game/AirplaneGame.java
@@ -9,40 +9,46 @@ import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
 
 
 public class AirplaneGame implements Screen {
 
-	SpriteBatch batch; // область для отрисовки спрайтов нашей игры
-	FPSLogger fpsLogger;
-	OrthographicCamera camera; // область просмотра нашей игры
+	private static SpriteBatch batch; // область для отрисовки спрайтов нашей игры
+	private static OrthographicCamera camera; // область просмотра нашей игры
 	MainGame game;
-	Viewport viewport;
+	private static Viewport viewport;
 
 	public AirplaneGame (MainGame game){
 
+		System.out.println("I'm in the GAME");
 		this.game = game;
 
 		float height = Gdx.graphics.getHeight();
 		float width = Gdx.graphics.getWidth();
 		camera = new OrthographicCamera(width,height);// устанавливаем переменные высоты и ширины в качестве области просмотра нашей игры
 		camera.setToOrtho(false);// этим методом мы центруем камеру на половину высоты и половину ширины экрана устройства
-		//camera.position.set(width/2, height/2,0);
-		viewport = new FitViewport(width,height, camera);
+		viewport = new FillViewport(width,height,camera);
 		batch = new SpriteBatch();
-		fpsLogger = new FPSLogger();
 		GameManager.initialize(width,height);
 	}
 
 	@Override
 	public void show() {
 
+		System.out.println("SHOW HERE");
+
 	}
 
 	@Override
 	public void render(float delta) {
+
+		System.out.println("RENDER HERE");
 
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -61,27 +67,39 @@ public class AirplaneGame implements Screen {
 	@Override
 	public void resize(int width, int height) {
 
+		System.out.println("RESIZE HERE");
+		System.out.println("width = " + width);
+		System.out.println("height = " + height);
 	    viewport.update(width,height);
+
+
 	}
 
 	@Override
 	public void pause() {
+
+		System.out.println("PAUSE HERE");
 
 	}
 
 	@Override
 	public void resume() {
 
+		System.out.println("RESUME HERE");
+
 	}
 
 	@Override
 	public void hide() {
 
+		System.out.println("HIDE HERE");
+		dispose();
 	}
 
 	@Override
 	public void dispose () {
 
+		System.out.println("DISPOSE HERE");
 		batch.dispose();
 		GameManager.dispose();
 
