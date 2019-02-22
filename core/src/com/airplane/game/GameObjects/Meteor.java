c1d558aef924f1c252ed28d1701ba9ef13ff8789
olliestyle <olliestyle1989@gmail.com>
2019-02-22 09:52:35 +0300

    page 72 - added comments

--- core/src/com/airplane/game/GameObjects/Meteor.java
+++ core/src/com/airplane/game/GameObjects/Meteor.java
@@ -6,23 +6,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 import static com.airplane.game.GameObjects.Plane.deltaTime;
+import static com.airplane.game.GameObjects.Plane.planeRect;
 
 public class Meteor {
 
-    private static Array<TextureAtlas.AtlasRegion> meteorTextures = new Array<TextureAtlas.AtlasRegion>();
-    private static TextureRegion selectedMeteorTexture;
-    private static boolean meteorInScene;
-    private static final int METEOR_SPEED=60;
-    private static Vector2 meteorPosition= new Vector2();
-    private static Vector2 meteorVelocity= new Vector2();
-    private static float nextMeteorIn;
+    private static Array<TextureAtlas.AtlasRegion> meteorTextures = new Array<TextureAtlas.AtlasRegion>(); //массив для хранения текстур метеоров
+    private static TextureRegion selectedMeteorTexture; // объект метеора, с которым будут проводиться взаимодействия
+    private static boolean meteorInScene; // для определения отображается ли в данный момент метеор?
+    private static final int METEOR_SPEED = 100; // скорость метеора
+    public static Vector2 meteorPosition= new Vector2(); // вектор позиции метеора
+    private static Vector2 meteorVelocity= new Vector2(); // вектор скорости метеора
+    public static float nextMeteorIn; // переменная, по которой определяем время поялвления следующего метеора
+    private static Rectangle meteorRect; // для коллизий
+    public static Vector2 destination = new Vector2();
 
     public static void initializeMeteor(){
 
+        /* добавляем в массив компоненты (все метеоры) нашего атласа текстур*/
         meteorTextures.add(GameManager.atlas.findRegion("meteorBrown_med1"));
         meteorTextures.add(GameManager.atlas.findRegion("meteorBrown_med2"));
         meteorTextures.add(GameManager.atlas.findRegion("meteorBrown_small1"));
@@ -30,11 +35,13 @@ public class Meteor {
         meteorTextures.add(GameManager.atlas.findRegion("meteorBrown_tiny1"));
         meteorTextures.add(GameManager.atlas.findRegion("meteorBrown_tiny2"));
 
-        nextMeteorIn = (float) (Math.random()*5);
+        nextMeteorIn = (float) (Math.random()*5); // высчитываем время до появления следующего метеора
+        meteorRect = new Rectangle(); // инициализируем прямоугольник
     }
 
     public static void renderMeteor(SpriteBatch batch){
 
+        /*отрисовываем метеор если он находится/отображается на экране*/
         if(meteorInScene)
         {
             batch.draw(selectedMeteorTexture,meteorPosition.x, meteorPosition.y);
@@ -42,18 +49,35 @@ public class Meteor {
     }
 
     public static void updateMeteor(){
-
+        /*если метеор находится на экране*/
         if(meteorInScene)
         {
-            meteorPosition.mulAdd(meteorVelocity, deltaTime);
-            meteorPosition.x -= Plane.planePosition.x - Plane.planeDefaultPosition.x;
+
+            meteorPosition.mulAdd(meteorVelocity, (float) (deltaTime*1.5)); // меняем позицию метеора на экране по этому вектору
+            //meteorPosition.x -= Plane.planePosition.x - Plane.planeDefaultPosition.x;
+
+            /*Устанавливаем область столкновения метеора в зависимости от его нынешней позиции на экране*/
+            meteorRect.set(meteorPosition.x, meteorPosition.y,selectedMeteorTexture.getRegionWidth()-4, selectedMeteorTexture.getRegionHeight()-4);
+
+            /*переходим в GAME_OVER при наложении области столкновения самолета и области столкновения метеора*/
+            if (planeRect.overlaps(meteorRect))
+            {
+                if (GameManager.gameState != GameManager.GameState.GAME_OVER){
+                    GameManager.gameState = GameManager.GameState.GAME_OVER;
+                }
+            }
+
+            /*Если метеор вылетел за левый край экрана - он больше не на экране*/
             if(meteorPosition.x<-10)
             {
                 meteorInScene=false;
             }
         }
-        nextMeteorIn-=deltaTime;
-        if(nextMeteorIn<=0)
+
+        /*Вызываем метод launchMeteor только если nextMeteorIn становится меньше 0.
+        * Но если метеор будет находится еще на экране то новый метеор не запускаем, а заново генерируем nextMeteorIn*/
+        nextMeteorIn -= deltaTime;
+        if(nextMeteorIn <= 0)
         {
             launchMeteor();
         }
@@ -68,13 +92,12 @@ public class Meteor {
             return;
         }
         meteorInScene=true;
-        int id= (int)(Math.random()*meteorTextures.size);
-        selectedMeteorTexture=meteorTextures.get(id);
-        meteorPosition.x= Gdx.graphics.getWidth();
-        meteorPosition.y=(float) (80+Math.random()*(Gdx.graphics.getHeight()-80));
-        Vector2 destination=new Vector2();
-        destination.x=-10;
-        destination.y=(float) (80+Math.random()*320);
+        int id= (int)(Math.random()*meteorTextures.size); // определяем, какой метеор взять из массива
+        selectedMeteorTexture = meteorTextures.get(id); // устанавливаем выбранную текстуру, относительно выбранного метеора
+        meteorPosition.x = Gdx.graphics.getWidth() + 20;
+        meteorPosition.y =(float) (20+Math.random()*(Gdx.graphics.getHeight()-20));
+        destination.x = -10;
+        destination.y = (float) (20+Math.random()*(Gdx.graphics.getHeight()-20));
         destination.sub(meteorPosition).nor();
         meteorVelocity.mulAdd(destination, METEOR_SPEED);
     }
--- core/src/com/airplane/game/Managers/TextManager.java
+++ core/src/com/airplane/game/Managers/TextManager.java
@@ -1,6 +1,7 @@
 package com.airplane.game.Managers;
 
 import com.airplane.game.AirplaneGame;
+import com.airplane.game.GameObjects.Meteor;
 import com.airplane.game.GameObjects.Plane;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
@@ -37,43 +38,61 @@ public class TextManager {
     public static void displayMessage(SpriteBatch batch){
 
         // объект класса GlyphLayout хранит в себе информацию о шрифте и содержании текста
+        /*
         GlyphLayout glyphLayoutHeight = new GlyphLayout();
         GlyphLayout glyphLayoutWidth = new GlyphLayout();
         GlyphLayout glyphLayoutTerrainOffset = new GlyphLayout();
+        */
         GlyphLayout glyphLayoutDeltaTime = new GlyphLayout();
+        /*
         GlyphLayout glyphLayoutPlaneAnimTime = new GlyphLayout();
         GlyphLayout glyphLayoutTouchPositionX = new GlyphLayout();
         GlyphLayout glyphLayoutTouchPositionY = new GlyphLayout();
         GlyphLayout glyphLayoutTapDrawTime = new GlyphLayout();
         GlyphLayout glyphLayoutPlanePostitionX = new GlyphLayout();
         GlyphLayout glyphLayoutPlanePostitionY = new GlyphLayout();
+        */
         GlyphLayout glyphLayoutINIT = new GlyphLayout();
         GlyphLayout glyphLayoutGAMEOVER = new GlyphLayout();
+        GlyphLayout glyphLayoutNextMeteorIn = new GlyphLayout();
 
+        /*
         glyphLayoutWidth.setText(font, "width (x) = " + Gdx.graphics.getWidth());
         glyphLayoutHeight.setText(font, "height (y) = " + Gdx.graphics.getHeight());
         glyphLayoutTerrainOffset.setText(font, "terrainOffset = " + GameManager.terrainOffset);
+        */
         glyphLayoutDeltaTime.setText(font, "deltaTime = " + Plane.deltaTime);
+        /*
         glyphLayoutPlaneAnimTime.setText(font, "planeAnimTime = " + Plane.planeAnimTime);
         glyphLayoutTouchPositionX.setText(font, "touchPosition X = " + InputManager.touchPosition.x);
         glyphLayoutTouchPositionY.setText(font, "touchPosition Y = " + InputManager.touchPosition.y);
         glyphLayoutTapDrawTime.setText(font, "tapDrawTime = " + Plane.tapDrawTime);
         glyphLayoutPlanePostitionX.setText(font, "planePositionX = " + Plane.planePosition.x);
         glyphLayoutPlanePostitionY.setText(font, "planePositionY = " + Plane.planePosition.y);
+        */
         glyphLayoutINIT.setText(font, "Tap next to the airplane to make it move ");
-        glyphLayoutGAMEOVER.setText(font, "Tap next to the airplane to make it move ");
+        //glyphLayoutGAMEOVER.setText(font, "Tap next to the airplane to make it move ");
         glyphLayoutGAMEOVER.setText(fontGameOver, "Game Over");
+        glyphLayoutNextMeteorIn.setText(font, "nextMeteorIn = " + Meteor.nextMeteorIn);
 
+        /*
         font.draw(batch, glyphLayoutWidth, (float) (width*0.01), (float) (height));
         font.draw(batch, glyphLayoutHeight, (float) (width*0.01), (float) (height*0.97));
         font.draw(batch, glyphLayoutTerrainOffset, (float) (width*0.01), (float) (height*0.94));
+        */
         font.draw(batch, glyphLayoutDeltaTime, (float) (width*0.01), (float) (height*0.91));
+        /*
         font.draw(batch, glyphLayoutPlaneAnimTime, (float) (width*0.01), (float) (height*0.88));
         font.draw(batch, glyphLayoutTouchPositionX, (float) (width*0.01), (float) (height*0.85));
         font.draw(batch, glyphLayoutTouchPositionY, (float) (width*0.01), (float) (height*0.82));
         font.draw(batch, glyphLayoutTapDrawTime, (float) (width*0.01), (float) (height*0.79));
         font.draw(batch, glyphLayoutPlanePostitionX, (float) (width*0.01), (float) (height*0.76));
         font.draw(batch, glyphLayoutPlanePostitionY, (float) (width*0.01), (float) (height*0.73));
+        */
+        font.draw(batch, glyphLayoutNextMeteorIn, (float) (width*0.01), (float) (height*0.97));
+
+
+
 
         if (gameState == GameManager.GameState.INIT)
             font.draw(batch, glyphLayoutINIT, (float) (width/3.4), (float) (height/2.1));
